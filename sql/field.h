b7820e7c03062213d9ff351ddc566580ed9a628a
rucsheng <rucsheng@gmail.com>
2018-08-14 23:54:00 +0800

    make m_null_ptr public & dec adjustable

--- sql/field.h
+++ sql/field.h
@@ -598,13 +598,15 @@ public:
 
   uchar		*ptr;			// Position to field in record
 
-private:
+/*TDSQL: make it public for columnstore*/
+public:
   /**
      Byte where the @c NULL bit is stored inside a record. If this Field is a
      @c NOT @c NULL field, this member is @c NULL.
   */
   uchar *m_null_ptr;
 
+private:
   /**
     Flag: if the NOT-NULL field can be temporary NULL.
   */
@@ -1778,7 +1780,7 @@ protected:
 
 class Field_num :public Field {
 public:
-  const uint8 dec;
+  /*const*/ uint8 dec;          // TDSQL:need to adjust this field due to vtable restriction
   bool zerofill,unsigned_flag;	// Purify cannot handle bit fields
   Field_num(uchar *ptr_arg,uint32 len_arg, uchar *null_ptr_arg,
 	    uchar null_bit_arg, utype unireg_check_arg,
